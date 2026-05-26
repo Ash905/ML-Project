003c9dcc7c847dc99dcd5633b2d9f3c1a39e1375
Ash905 <ashutoshtrivedi9632@gmail.com>
2026-05-26 13:12:48 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,10 @@
-import os 
+import os
 import sys
+from pathlib import Path
+
+# Add project root to Python path
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
+
 import src.exception as CustomException
 from src.logger import logging
 import pandas as pd
@@ -21,5 +26,28 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
-            logging.info("Read the dataset as dataframe")
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info("Read the dataset as dataframe")  
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) # This will create the directory if it does not exist, and if it already exists, it will do nothing (exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) # Raw data is stored in raw_data_path
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42) # This will split the data into train and test set with 80% and 20% respectively
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) # Train data is stored in train_data_path
+            
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True) # Test data is stored in test_data_path
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+if __name__ =="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
